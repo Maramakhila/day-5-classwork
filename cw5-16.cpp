9c1a44c305b0d5c8268590baf1f045ec0e2beb37
Maramakhila <130456749+Maramakhila@users.noreply.github.com>
2023-04-18 18:21:26 +0530

    Add files via upload

---
+++ cw5-16.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isPrime(int n, int i);
+
+int main() {
+    int n;
+
+    printf("Input any positive number: ");
+    scanf("%d", &n);
+
+    if (isPrime(n, n / 2) == 1) {
+        printf("%d is a prime number.\n", n);
+    } else {
+        printf("%d is not a prime number.\n", n);
+    }
+
+    return 0;
+}
+
+int isPrime(int n, int i) {
+    if (i == 1) {
+        return 1;
+    } else {
+        if (n % i == 0) {
+            return 0;
+        } else {
+            return isPrime(n, i - 1);
+        }
+    }
+}
